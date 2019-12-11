perf101/ocaml-sql

commit 53ab8e3820b4e3f3d6bb1b3519bc3170c9153525

Use postgresql builtin escaping
Signed-off-by: Edwin Török <edvin.torok@citrix.com>

sql.ml
@@ -124,9 +124,6 @@ let get_col_type ~conn ~tbl ~col_name =
 let get_col_names ~conn ~tbl =
   List.map fst (get_col_types_lst ~conn ~tbl)
 
-let escape_single_quotes str =
-  Str.global_replace (Str.regexp "'") "''" str
-
 let check_value ~ty ~value =
   match ty with
   | Type.Double ->
@@ -140,7 +137,7 @@ let string_of_value ~conn ~tbl ~col_name ~value =
   check_value ~ty ~value;
   match Type.is_quoted ty with
   | false -> if value="None" then "NULL" else value
-  | true -> "'" ^ (escape_single_quotes value) ^ "'"
+  | true -> conn#escape_string value
 
 let sql_values_of_tuples ~conn ~tbl ~tuples =
   let value_string_of_tuple (col_name, value) =

dune
@@ -1,5 +1,6 @@
 (library
  (name sql)
  (libraries postgresql)
- (flags (:standard -w @a-4-6-9-27-28-29))
+ (flags
+  (:standard -w @a-4-6-9-27-28-29))
  (wrapped false))

commit 3ea0bf981e0a51c4dbf1138e89eb7cd887ecf39a

Properly quote escaped string
This got broken by 53ab8e3820b4e3f3d6bb1b3519bc3170c9153525

Signed-off-by: Edwin Török <edvin.torok@citrix.com>

sql.ml
@@ -137,7 +137,7 @@ let string_of_value ~conn ~tbl ~col_name ~value =
   check_value ~ty ~value;
   match Type.is_quoted ty with
   | false -> if value="None" then "NULL" else value
-  | true -> conn#escape_string value
+  | true -> "'" ^ (conn#escape_string value) ^ "'"
 
 let sql_values_of_tuples ~conn ~tbl ~tuples =
   let value_string_of_tuple (col_name, value) =